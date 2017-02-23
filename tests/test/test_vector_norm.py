c60aed38f8881b47f9d7b4380860072684fea32f
julian <julian@noyce.uclm.es>
2017-02-23 17:09:49 +0100

    Added time feature

--- tests/test/test_vector_norm.py
+++ tests/test/test_vector_norm.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8; mode: python -*-
 
-from vector_norm import scale2, mult_hist_scale, sum_hist_pow
-import unittest
-import time
 
-class TestL2Norm(unittest.TestCase):
+
+import unittest
+from unittest_time import TimeTestCase
+
+from vector_norm import scale2, mult_hist_scale, sum_hist_pow
+
+
+class TestL2Norm(TimeTestCase):
 
     def test_scale2(self):
+        self.CONFIGURE_UNITTEST_TIME_HW_ADDR(0x41000000);
+        self.CONFIGURE_UNITTEST_TIME_INPUT_32BIT_WORDS(3);
+        self.CONFIGURE_UNITTEST_TIME_OUTPUT_32BIT_WORDS(1);
+
+        self.UNITTEST_TIME_START();
         self.assertEqual(0.21739131212234497, scale2(9.0))
+        self.assertTimeLT(40)
+        self.assertTimeGT(30)
         
 
     def test_sum_hist_pow(self):
         din = []
         for i in range(0,16):
             din.append(float(i))
+        self.CONFIGURE_UNITTEST_TIME_HW_ADDR(0x41000000);
+        self.CONFIGURE_UNITTEST_TIME_INPUT_32BIT_WORDS(18);
+        self.CONFIGURE_UNITTEST_TIME_OUTPUT_32BIT_WORDS(1);
+
+        self.UNITTEST_TIME_START();
         self.assertEqual(1240.0, sum_hist_pow(din))
+        self.assertTimeGT(50)
 
     def test_mult_hist_scale(self):
         din = []
@@ -24,9 +41,18 @@ class TestL2Norm(unittest.TestCase):
         for i in range(0,16):
             din.append(float(i))
             ref.append(float(i)*0.1)
-        
-        mult_hist_scale(din, 0.1, dout)
 
+        
+        self.CONFIGURE_UNITTEST_TIME_HW_ADDR(0x41000000);
+        self.CONFIGURE_UNITTEST_TIME_INPUT_32BIT_WORDS(18);
+        self.CONFIGURE_UNITTEST_TIME_OUTPUT_32BIT_WORDS(1);
+
+        self.UNITTEST_TIME_START();
+        mult_hist_scale(din, 0.1, dout)
+        
+        self.assertTimeLT(250)
+        self.assertTimeGT(220)
+        
         for o,r in zip(dout,ref):
             self.assertEqual(r,o)
 
--- tests/vector_norm.py
+++ tests/vector_norm.py
@@ -1,62 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8; mode: python -*-
 
-import struct
 
+
+from hwt_proxy import FPGA_hwtClient
+from arm_casting_ieee754 import *
 
 import sys
-import Ice
-
-Ice.loadSlice("/opt/hw_testing/tools/slices/testingService.ice")
-import TestingService
-
-fpga_endpoint = 'GCommand -e 1.0 -t:tcp -h zynq-kilby.uclm.es -p 7891'
-
-
-class Client(Ice.Application):
-    def arguments(self, din):
-        self.din = din
-        self.dout = None
-
-    def result(self):
-        return self.dout      
-
-        
-    def run(self, args):        
-        ic = self.communicator()
-
-        proxy = ic.stringToProxy(fpga_endpoint)
-        proxy = TestingService.GCommandPrx.uncheckedCast(proxy)
-        self.dout=proxy.remoteExec(0x42000000, self.din)
-
-    
-def float_to_ieee754(f):
-    return struct.unpack('<I', struct.pack('<f', f))[0]
-
-def ieee754_to_float(ieee):
-    return (struct.unpack('f', struct.pack('<I', int(ieee,16)))[0])
-
-
-def charSeq_to_intSeq(cseq):
-    iseq = []
-    it_index = 1
-    aux = []
-    for it in cseq:
-        aux.append(int(it))
-        it_index += 1
-        if it_index%4 == 1:
-            iseq.append(hex(struct.unpack("<I",  bytearray(aux))[0]))
-            aux = []
-    return iseq
-
-
-def int_to_byte(n):
-    b = []
-    for it in range(0,4):
-        b.append(n&0xFF)
-        n >>= 8
-    return b
-
 
 
 def scale2(sum):
@@ -65,12 +15,14 @@ def scale2(sum):
     din.extend(int_to_byte(0x00000001))
     din.extend(int_to_byte(float_to_ieee754(sum)))
 
-    testCli = Client()
-    testCli.arguments(din)
-    testCli.main([fpga_endpoint])
+    print(din)
+    testCli = FPGA_hwtClient()
+    testCli.arguments(0x42000000, din)
+    testCli.main([None])#[fpga_endpoint])
     dout = testCli.result()
     idout = charSeq_to_intSeq(dout)
 
+    print (idout)
     head1 = idout[0]
     head2 = idout[1]
     _ret = ieee754_to_float(idout[2])
@@ -80,6 +32,7 @@ def scale2(sum):
     
     return _ret
 
+
 def sum_hist_pow(histIN):
     din = []
     din.extend(int_to_byte(0x00010204))
@@ -88,9 +41,9 @@ def sum_hist_pow(histIN):
     for it in histIN:
         din.extend(int_to_byte(float_to_ieee754(it)))
     
-    testCli = Client()
-    testCli.arguments(din)
-    testCli.main([fpga_endpoint])
+    testCli = FPGA_hwtClient()
+    testCli.arguments(0x42000000, din)
+    testCli.main([None])#[fpga_endpoint])
     dout = testCli.result()
     idout = charSeq_to_intSeq(dout)
     
@@ -111,9 +64,9 @@ def mult_hist_scale(histAUX, scale, histOUT):
         
     din.extend(int_to_byte(float_to_ieee754(scale)))
         
-    testCli = Client()
-    testCli.arguments(din)
-    testCli.main([fpga_endpoint])
+    testCli = FPGA_hwtClient()
+    testCli.arguments(0x42000000, din)
+    testCli.main([None])#[fpga_endpoint])
     dout = testCli.result()
     idout = charSeq_to_intSeq(dout)
     
