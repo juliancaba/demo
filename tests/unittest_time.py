153f508852a0357f83a266c7e803d2177f5f523b
julian <julian@noyce.uclm.es>
2017-03-24 12:13:26 +0100

    Updated Unittest framework

--- tests/unittest_time.py
+++ tests/unittest_time.py
@@ -17,22 +17,45 @@ class TimeTestCase(TestCase):
       self._time_hw_addr = 0x41000000
       self._inputWords = 0x0001
       self._outputWords = 0x0001
+      self._enableCycles = 0x00001000
 
 
     def _unittest_TimeReset(self):
       self._inputWords = 0x0001
       self._outputWords = 0x0001
       self._time_hw_addr = 0x41000000
+      self._enableCycles = 0x00001000
+
+      din = []
+      din.extend(int_to_byte(0x00010100))
+      din.extend(int_to_byte(0x00000000))
+
+      testCli = FPGA_hwtClient()
+      testCli.arguments(self._time_hw_addr, din)
+      testCli.main([None])
+      dout = testCli.result()
+      idout = charSeq_to_intSeq(dout)
+
+      head1 = idout[0]
+
+      del testCli
+      del idout
 
 
     def CONFIGURE_UNITTEST_TIME_HW_ADDR(self, addr):
       self._time_hw_addr = addr
 
+    def CONFIGURE_UNITTEST_TIME_ENABLE_CYCLES(self, n):
+      self._enableCycles = n
+      
     def CONFIGURE_UNITTEST_TIME_INPUT_32BIT_WORDS(self, n):
       self._inputWords = n
     
     def CONFIGURE_UNITTEST_TIME_OUTPUT_32BIT_WORDS(self, n):
       self._outputWords = n
+
+    def UNITTEST_TIME_RESET(self):
+        self._unittest_TimeReset()
 
     def UNITTEST_TIME_START(self):
       global _time_value
@@ -42,6 +65,28 @@ class TimeTestCase(TestCase):
       din = []
       din.extend(int_to_byte(0x00010204))
       din.extend(int_to_byte(0x00000001))
+      din.extend(int_to_byte((self._inputWords << 16) | self._outputWords))
+
+      testCli = FPGA_hwtClient()
+      testCli.arguments(self._time_hw_addr, din)
+      testCli.main([None])
+      dout = testCli.result()
+      idout = charSeq_to_intSeq(dout)
+
+      head1 = idout[0]
+
+      del testCli
+      del idout
+
+    def UNITTEST_TIME_CONFIGURE(self):
+      global _time_value
+      global _time_valid
+      _time_value = 0
+      _time_valid = 0
+      din = []
+      din.extend(int_to_byte(0x00010204))
+      din.extend(int_to_byte(0x00000002))
+      din.extend(int_to_byte(self._enableCycles))
       din.extend(int_to_byte((self._inputWords << 16) | self._outputWords))
 
       testCli = FPGA_hwtClient()
