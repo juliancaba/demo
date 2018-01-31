efd7bb180fbb16123a521296dfd0e922ef0d44be
julian <julian@shockley>
2018-01-31 10:44:15 +0100

    New macros

--- tests/test/test_vector_norm.py
+++ tests/test/test_vector_norm.py
@@ -4,21 +4,21 @@
 
 
 import unittest
-from unittest_time import TimeTestCase
+from rc_unittest_remote import RCUnittestTestCase
 
 from vector_norm import scale2, mult_hist_scale, sum_hist_pow
 
 
-class TestL2Norm(TimeTestCase):
+class TestL2Norm(RCUnittestTestCase):
 
     def test_scale2(self):
-        self.UNITTEST_TIME_RESET()
-        self.CONFIGURE_UNITTEST_TIME_HW_ADDR(0x41000000)
-        self.CONFIGURE_UNITTEST_TIME_INPUT_32BIT_WORDS(3)
-        self.CONFIGURE_UNITTEST_TIME_OUTPUT_32BIT_WORDS(1)
-        self.CONFIGURE_UNITTEST_TIME_ENABLE_CYCLES(2000000)
+        self.TEST_RESET()
+        self.CONFIGURE_HW_ADDR(0x41000000)
+        self.CONFIGURE_IGNORE_INPUT(3)
+        self.CONFIGURE_IGNORE_OUTPUT(1)
+        self.CONFIGURE_ENABLE_CYCLES(2000000)
 
-        self.UNITTEST_TIME_CONFIGURE();
+        self.TEST_CONFIGURE();
         self.assertEqual(0.21739131212234497, scale2(9.0))
         self.assertTimeLT(40)
         self.assertTimeGT(20)
@@ -29,13 +29,13 @@ class TestL2Norm(TimeTestCase):
         for i in range(0,16):
             din.append(float(i))
             
-        self.UNITTEST_TIME_RESET()
-        self.CONFIGURE_UNITTEST_TIME_HW_ADDR(0x41000000)
-        self.CONFIGURE_UNITTEST_TIME_INPUT_32BIT_WORDS(18)
-        self.CONFIGURE_UNITTEST_TIME_OUTPUT_32BIT_WORDS(1)
-        self.CONFIGURE_UNITTEST_TIME_ENABLE_CYCLES(2000000)
+        self.TEST_RESET()
+        self.CONFIGURE_HW_ADDR(0x41000000)
+        self.CONFIGURE_IGNORE_INPUT(18)
+        self.CONFIGURE_IGNORE_OUTPUT(1)
+        self.CONFIGURE_ENABLE_CYCLES(2000000)
 
-        self.UNITTEST_TIME_CONFIGURE();
+        self.TEST_CONFIGURE();
         self.assertEqual(1240.0, sum_hist_pow(din))
         self.assertTimeGT(20)
 
@@ -47,13 +47,13 @@ class TestL2Norm(TimeTestCase):
             din.append(float(i))
             ref.append(float(i)*0.1)
 
-        self.UNITTEST_TIME_RESET();
-        self.CONFIGURE_UNITTEST_TIME_HW_ADDR(0x41000000)
-        self.CONFIGURE_UNITTEST_TIME_INPUT_32BIT_WORDS(18)
-        self.CONFIGURE_UNITTEST_TIME_OUTPUT_32BIT_WORDS(1)
-        self.CONFIGURE_UNITTEST_TIME_ENABLE_CYCLES(2000000)
+        self.TEST_RESET();
+        self.CONFIGURE_HW_ADDR(0x41000000)
+        self.CONFIGURE_IGNORE_INPUT(18)
+        self.CONFIGURE_IGNORE_OUTPUT(1)
+        self.CONFIGURE_ENABLE_CYCLES(2000000)
 
-        self.UNITTEST_TIME_CONFIGURE();
+        self.TEST_CONFIGURE();
         mult_hist_scale(din, 0.1, dout)
         
         self.assertTimeLT(350)
